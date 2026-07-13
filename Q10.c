b1cb9cabf630d50887c3432a3aabcd7b74a352dd
nishkarshsonker56-rgb <nishkarshsonker56@gmail.com>
2026-07-13 20:56:16 +0530

    Create Q10.c

---
+++ Q10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, i, j, prime;
+
+    printf("Enter start and end: ");
+    scanf("%d %d", &start, &end);
+
+    for (i = start; i <= end; i++) {
+        if (i < 2)
+            continue;
+
+        prime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+
+        if (prime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
